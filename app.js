424e7f5e24ce835b2609a941a38bf8bdc9f4743b
josemariatafoyalvarado <josemariatafoyalvarado@gmail.com>
2025-11-06 19:37:07 -0600

    cambio grande

--- app.js
+++ app.js
@@ -107,8 +107,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     } else {
         console.warn("GSAP o ScrollTrigger no están cargados. Las animaciones por scroll no se ejecutarán.");
-    };
+    }
 
+    // Configuración de Firebase
+        const firebaseConfig = {
+    apiKey: "__FIREBASE_API_KEY__",
+    authDomain: "__FIREBASE_AUTH_DOMAIN__",
+    projectId: "__FIREBASE_PROJECT_ID__",
+    storageBucket: "__FIREBASE_STORAGE_BUCKET__",
+    messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
+    appId: "__FIREBASE_APP_ID__",
+    measurementId: "__FIREBASE_MEASUREMENT_ID__"
+    };
 
     let auth;
     let db; 
--- config.js
+++ config.js
@@ -1,10 +1,1 @@
 // config.js - ESTE ARCHIVO ES TU SECRETO. ¡NO DEBE SUBIRSE A GITHUB!
-    const firebaseConfig = {
-      apiKey: "AIzaSyAAXJ-wklT3mfxdQO16DDwmAriYxroiEKA",
-      authDomain: "cursos-7ae54.firebaseapp.com",
-      projectId: "cursos-7ae54",
-      storageBucket: "cursos-7ae54.appspot.com",
-      messagingSenderId: "cursos-7ae54.appspot.com",
-      appId: "1:356389037138:web:bdc9cd1c2a8acfd2bbab8b",
-      measurementId: "G-XV8TV6P3Z1"
-    };
