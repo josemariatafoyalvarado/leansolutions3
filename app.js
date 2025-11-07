c7a16ee0f03da567d557e1488544c0d26e7c1fed
josemariatafoyalvarado <josemariatafoyalvarado@gmail.com>
2025-11-06 20:00:39 -0600

    cambiesotes

--- app.js
+++ app.js
@@ -111,13 +111,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Configuración de Firebase
         const firebaseConfig = {
-    apiKey: "__FIREBASE_API_KEY__",
-    authDomain: "__FIREBASE_AUTH_DOMAIN__",
-    projectId: "__FIREBASE_PROJECT_ID__",
-    storageBucket: "__FIREBASE_STORAGE_BUCKET__",
-    messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
-    appId: "__FIREBASE_APP_ID__",
-    measurementId: "__FIREBASE_MEASUREMENT_ID__"
+    apiKey: __FIREBASE_API_KEY__,
+    authDomain: __FIREBASE_AUTH_DOMAIN__,
+    projectId: __FIREBASE_PROJECT_ID__,
+    storageBucket: __FIREBASE_STORAGE_BUCKET__,
+    messagingSenderId: __FIREBASE_MESSAGING_SENDER_ID__,
+    appId: __FIREBASE_APP_ID__,
+    measurementId: __FIREBASE_MEASUREMENT_ID__
     };
 
     let auth;
